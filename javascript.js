1b7fb7afd18dc8c53887035bd8998def79d5c0ac
nxm1137 <60846613+nxm1137@users.noreply.github.com>
2021-03-21 18:23:16 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,35 @@
+            // NUMBER ONE
+            function imgOnClick(){
+                document.getElementById("myModal").style.display = "block";
+                document.getElementById("img01").src = myImg1.src ;
+            }
+            function spanOnClick() { 
+                document.getElementById("myModal").style.display = "none";
+            }
+                 
+            // NUMBER TWO
+            function imgOnClick2(){
+                document.getElementById("myModal2").style.display = "block";
+                document.getElementById("img02").src = myImg2.src ;
+            }
+            function spanOnClick2() { 
+                document.getElementById("myModal2").style.display = "none";
+            }
+
+            // NUMBER THREE
+            function imgOnClick3(){
+                document.getElementById("myModal3").style.display = "block";
+                document.getElementById("img03").src = myImg3.src ;
+            }
+            function spanOnClick3() { 
+                document.getElementById("myModal3").style.display = "none";
+            }
+                 
+            // NUMBER FOUR
+            function imgOnClick4(){
+                document.getElementById("myModal4").style.display = "block";
+                document.getElementById("img04").src = myImg4.src ;
+            }
+            function spanOnClick4() { 
+                document.getElementById("myModal4").style.display = "none";
+            }
